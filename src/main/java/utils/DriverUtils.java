877fdc5bcce7d1e0287432cc5c7c69f4135216ae
MelihDemirtel <melihcandemirtel@gmail.com>
2023-04-10 00:45:47 +0300

    added DriverUtils.java

---
+++ src/main/java/utils/DriverUtils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class DriverUtils {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
