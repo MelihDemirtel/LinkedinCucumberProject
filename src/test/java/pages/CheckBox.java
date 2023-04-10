2f51c6cce7f94fa515666879f05abf02b6e7b571
MelihDemirtel <melihcandemirtel@gmail.com>
2023-04-10 13:26:30 +0300

    checkBox project done

--- src/test/java/pages/CheckBox.java
+++ src/test/java/pages/CheckBox.java
@@ -2,7 +2,9 @@ package pages;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -15,5 +17,27 @@ public class CheckBox extends CheckBoxWebElements{
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         this.jsx = (JavascriptExecutor) driver;
+    }
+    public void homePage() {
+        jsx.executeScript("window.scrollBy(0,300)");
+        wait.until(ExpectedConditions.presenceOfElementLocated(homePage));
+        System.out.println("User At Home Page");
+    }
+
+    public void clickElements(){
+       elementsButton.click();
+    }
+
+    public void clickCheckBox(){
+        checkBoxButton.click();
+    }
+
+    public void clickHomeCheckBox(){
+        homeCheckBoxButton.click();
+    }
+
+    public void assertInfo(){
+        Assert.assertTrue(actualResult.isDisplayed());
+        System.out.println(actualResult.getText());
     }
 }
