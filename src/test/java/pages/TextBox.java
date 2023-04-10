d90c233a8a952c2ddd7eab0cbf8b07bebdf8fe78
MelihDemirtel <melihcandemirtel@gmail.com>
2023-04-10 11:58:05 +0300

    added webElements page

--- src/test/java/pages/TextBox.java
+++ src/test/java/pages/TextBox.java
@@ -1,35 +1,17 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
-public class TextBox {
+public class TextBox extends TextBoxWebElements{
     static WebDriver driver;
     static WebDriverWait wait;
     static JavascriptExecutor jsx;
-
-    static String fullName = "Test";
-    static String email = "test@deneme.com";
-    static String currentAddress = "Test, Test/Deneme";
-
-    static By homePage = By.xpath("//div[@class='category-cards']");
-    static By elementsButton = By.xpath("//h5[text()='Elements']");
-    static By textBoxButton = By.xpath("//span[text()='Text Box']");
-    static By fullNameLabel = By.id("userName");
-    static By emailLabel = By.id("userEmail");
-    static By currentAddressLabel = By.id("currentAddress");
-    static By submitButton = By.id("submit");
-
-    static By actualResultName = By.id("name");
-    static By actualResultEmail = By.id("email");
-    static By actualResultAddress = By.xpath("//p[@id='currentAddress']");
 
     public TextBox(WebDriver driver) {
         this.driver = driver;
---
+++ src/test/java/pages/TextBoxWebElements.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class TextBoxWebElements {
+
+    static String fullName = "Test";
+    static String email = "test@deneme.com";
+    static String currentAddress = "Test, Test/Deneme";
+
+    static By homePage = By.xpath("//div[@class='category-cards']");
+    static By elementsButton = By.xpath("//h5[text()='Elements']");
+    static By textBoxButton = By.xpath("//span[text()='Text Box']");
+    static By fullNameLabel = By.id("userName");
+    static By emailLabel = By.id("userEmail");
+    static By currentAddressLabel = By.id("currentAddress");
+    static By submitButton = By.id("submit");
+
+    static By actualResultName = By.id("name");
+    static By actualResultEmail = By.id("email");
+    static By actualResultAddress = By.xpath("//p[@id='currentAddress']");
+}
