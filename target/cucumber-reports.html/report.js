4fa6d4bcb12ed7830ad930757f61f23e128351c5
MelihDemirtel <melihcandemirtel@gmail.com>
2023-04-15 14:30:11 +0300

    webTables scenario-2 is done, will make scenario-3

--- target/cucumber-reports.html/report.js
+++ target/cucumber-reports.html/report.js
@@ -1,323 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/CheckBox.feature");
-formatter.feature({
-  "name": "CheckBox",
-  "description": "  As a user\n  I want to click check boxes\n  So that I can see boxes",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "name": "@CheckBox"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.scenario({
-  "name": "Scenario-1 Click Check Boxes",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@CheckBox"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I am on the demoqa home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "StepDefinitions.iAmOnTheDemoqaHomePage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click elements page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickElementsPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click check box page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickCheckBoxPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click home check box",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickHomeCheckBox()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user checked boxes",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "StepDefinitions.userCheckedBoxes()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("file:src/test/java/features/RadioButton.feature");
-formatter.feature({
-  "name": "RadioButton",
-  "description": "  As a user\n  I want to click radio buttons\n  So that I can see messages",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "name": "@RadioButton"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.scenario({
-  "name": "Scenario-1 Click Radio Buttons",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@RadioButton"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I am on the demoqa home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "StepDefinitions.iAmOnTheDemoqaHomePage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click elements page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickElementsPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click radio button page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickRadioButtonPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click yes button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickYesButton()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user check yes messages",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "StepDefinitions.userCheckYesMessages()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click impressive button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickImpressiveButton()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user check impressive messages",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "StepDefinitions.userCheckImpressiveMessages()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user tries to click no button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userTriesToClickNoButton()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user sees that the button cannot be clicked",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "StepDefinitions.userSeesThatTheButtonCannotBeClicked()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("file:src/test/java/features/TextBox.feature");
-formatter.feature({
-  "name": "TextBox",
-  "description": "  As a user\n  I want to submit information form\n  So that I can write my information",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "name": "@TextBox"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.scenario({
-  "name": "Scenario-1 Submit to information of user",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@TextBox"
-    },
-    {
-      "name": "@Smoke"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I am on the demoqa home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "StepDefinitions.iAmOnTheDemoqaHomePage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click elements page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickElementsPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click text box page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickTextBoxPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user write full name",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userWriteFullName()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user write email",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userWriteEmail()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user write current address",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "StepDefinitions.userWriteCurrentAddress()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user click submit",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "StepDefinitions.userClickSubmit()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user check information",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "StepDefinitions.userCheckInformation()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("file:src/test/java/features/WebTables.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/WebTables.feature");
 formatter.feature({
   "name": "WebTables",
   "description": "  As a user\n  I want to add my information and remove my information in the web table\n  So that I can write my information in registration form and delete my information",
@@ -507,6 +188,16 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
+  "name": "user click web tables",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinitions.userClickWebTables()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
   "name": "user click delete button",
   "keyword": "When "
 });
@@ -514,8 +205,7 @@ formatter.match({
   "location": "StepDefinitions.userClickDeleteButton()"
 });
 formatter.result({
-  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#delete\\-record\\-1\"}\n  (Session info: chrome\u003d111.0.5563.65)\nFor documentation on this error, please visit: https://selenium.dev/exceptions/#no_such_element\nBuild info: version: \u00274.8.3\u0027, revision: \u0027e5e76298c3\u0027\nSystem info: os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002711.0.12\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [dcf3b80714bdb775ac5591ddd13e2cb5, findElement {using\u003did, value\u003ddelete-record-1}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 111.0.5563.65, chrome: {chromedriverVersion: 111.0.5563.64 (c710e93d5b63..., userDataDir: C:\\Users\\EXT02D~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:58421}, networkConnectionEnabled: false, pageLoadStrategy: normal, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:58421/devtoo..., se:cdpVersion: 111.0.5563.65, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: dcf3b80714bdb775ac5591ddd13e2cb5\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:193)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:183)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:543)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:66)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:352)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:344)\r\n\tat pages.WebTables.clickDeleteButton(WebTables.java:66)\r\n\tat stepDefinitions.StepDefinitions.userClickDeleteButton(StepDefinitions.java:144)\r\n\tat ✽.user click delete button(file:src/test/java/features/WebTables.feature:25)\r\n",
-  "status": "failed"
+  "status": "passed"
 });
 formatter.step({
   "name": "user check first row in the table",
@@ -525,7 +215,7 @@ formatter.match({
   "location": "StepDefinitions.userCheckFirstRowInTheTable()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.after({
   "status": "passed"
